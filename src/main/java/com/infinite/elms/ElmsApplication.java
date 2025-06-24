772c058fb8680856a2b10df6acc9b698b0eccf94
Bijay Adhikari <bijay.adhikari1000@gmail.com>
2025-06-24 17:06:09 +0545

    Initial Setup

---
+++ src/main/java/com/infinite/elms/ElmsApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.elms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmsApplication.class, args);
+	}
+
+}
