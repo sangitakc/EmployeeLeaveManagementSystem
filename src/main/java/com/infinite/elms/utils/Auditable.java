8b94225a252e7b1aeca77c4a678320cb451e9a8a
Bijay Adhikari <bijay.adhikari1000@gmail.com>
2025-06-24 22:13:01 +0545

    Add auditable class

---
+++ src/main/java/com/infinite/elms/utils/Auditable.java
@@ -0,0 +1,25 @@
+package com.infinite.elms.utils;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
