37b4d2ba4a9e2fc06cb19c2fdd09c050c23cb757
Bijay Adhikari <bijay.adhikari1000@gmail.com>
2025-06-24 23:33:55 +0545

    Add leave request model

---
+++ src/main/java/com/infinite/elms/models/LeaveRequest.java
@@ -0,0 +1,47 @@
+package com.infinite.elms.models;
+
+import com.infinite.elms.constants.LeaveStatus;
+import com.infinite.elms.constants.LeaveType;
+import com.infinite.elms.utils.Auditable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "leave_requests")
+@Builder
+public class LeaveRequest extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate requestDate;
+
+    private LocalDate endDate;
+
+    private LocalDate decisionDate;
+
+    private String decisionComment;
+
+    @Enumerated(EnumType.STRING)
+    private LeaveStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LeaveType leaveType;
+
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private User employee;
+
+    @ManyToOne
+    @JoinColumn(name = "approver_id")
+    private User approver;
+}
