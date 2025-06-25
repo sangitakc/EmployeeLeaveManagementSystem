d4e82ea3150e4e2b395466482fb58a1b06d4cd5d
Sabina Saru <sabina.saru811@gmail.com>
2025-06-25 16:53:12 +0545

    Add Leave Request DTO

---
+++ src/main/java/com/infinite/elms/dtos/LeaveRequestDTO.java
@@ -0,0 +1,22 @@
+package com.infinite.elms.dtos;
+
+import com.infinite.elms.constants.LeaveType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class LeaveRequestDTO {
+    @NotNull(message = "Start date is required")
+    @FutureOrPresent(message = "Start date cannot be in the past")
+    private LocalDate endDate;
+
+    @NotNull(message = "Leave type is required")
+    private LeaveType leaveType;
+
+    @Size(max = 500, message = "Reason cannot exceed 500 characters")
+    private String reason;
+}
