2d49c80206e609cd13e2a70bb224c6a086ac31c5
Sabina Saru <sabinas@infinite.com>
2025-06-25 13:00:49 +0545

    Add user repository

---
+++ src/main/java/com/infinite/elms/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.infinite.elms.repositories;
+
+import com.infinite.elms.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
